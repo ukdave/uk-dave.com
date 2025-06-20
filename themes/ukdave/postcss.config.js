af37e4e1a7f45559be2beb0e7d2d61ce77e86e67
David Bull <david@uk-dave.com>
2025-06-20 23:24:46 +0100

    Update Javascript dependencies

--- themes/ukdave/postcss.config.js
+++ themes/ukdave/postcss.config.js
@@ -2,16 +2,14 @@ const purgecss = require("@fullhuman/postcss-purgecss");
 
 module.exports = {
   plugins: [
-    purgecss({
+    purgecss.default({
       content: ["./hugo_stats.json"],
       safelist: {
         deep: [/chroma/]
       },
-      options: {
-        defaultExtractor: (content) => {
-          let els = JSON.parse(content).htmlElements;
-          return els.tags.concat(els.classes, els.ids);
-        },
+      defaultExtractor: (content) => {
+        let els = JSON.parse(content).htmlElements;
+        return els.tags.concat(els.classes, els.ids);
       },
     }),
     require("autoprefixer"),
